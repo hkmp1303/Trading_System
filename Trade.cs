34711c910c2a44e5e2455988a9f00fa0b138b4e9
hkmp1303 <hkmp1303@gmail.com>
2025-10-06 01:32:59 +0200

    implemented create trade
    minimal system requirement

--- Trade.cs
+++ Trade.cs
@@ -4,11 +4,14 @@ class Trade
 {
     public Trade_State Status; // trade status of indiviudal trades
     public Item Item;
+    public Item OwnItem;
 
     public static Dictionary<string, List<Trade>> tradesByUser = new Dictionary<string, List<Trade>>();
-    public Trade(string username, Item item, Trade_State status) // trade object constructor
+    private static Trade_State trade_State;
+    public Trade(string username, Item item, Item ownItem, Trade_State status) // trade object constructor
     {
         Item = item;
+        OwnItem = ownItem;
         Status = status;
     }
 
@@ -16,7 +19,7 @@ class Trade
     {
         while (true)
         {
-            Trade_State trade_State = Trade_State.SHOW_Menu;
+            trade_State = Trade_State.SHOW_Menu;
             switch (trade_State)
             {
                 case Trade_State.SHOW_Menu:
@@ -24,6 +27,7 @@ class Trade
                     switch (Console.ReadLine() ?? "")
                     {
                         case "t":  // request trade
+                            startTrade(loggedInUser);
                             break;
                         case "p":   // accept, deny requests
                             break;
@@ -37,6 +41,61 @@ class Trade
             }
         }
     }
+
+    public static void startTrade(string loggedInUser)
+    {
+        System.Console.WriteLine("");
+        Dictionary<int, KeyValuePair<string, Item>> tradeItems = new Dictionary<int, KeyValuePair<string, Item>>();
+        int i = 0;
+        foreach (var userItems in Item.itemsByUser)
+        {
+            if (userItems.Key == loggedInUser) continue; // skip own items
+            foreach (Item item in userItems.Value)
+            {
+                tradeItems.Add(i++, new KeyValuePair<string, Item>(userItems.Key, item));
+                System.Console.WriteLine(i + ". " + userItems.Key + "\t" + item.Name + "\t" + item.Description);
+            }
+        }
+        System.Console.WriteLine("Select a numbered item to trade or 0 to cancel");
+        int itemSelection = 0;
+        if (!int.TryParse(Console.ReadLine() ?? "0", out itemSelection))
+        {
+            itemSelection = 0;
+        }
+        if (itemSelection == 0)
+        {
+            trade_State = Trade_State.SHOW_Menu;
+            System.Console.WriteLine("Trade canceled. ");
+            return;
+        }
+        List<Trade> userTrades;
+        string username = tradeItems[itemSelection - 1].Key; // username of selected item
+        if (!tradesByUser.TryGetValue(loggedInUser, out userTrades)) // Verify list exists for logged in user
+        {
+            userTrades = new List<Trade>(); // create missing list if needed
+            tradesByUser.Add(loggedInUser, userTrades); // add missing list to tradesByUser index
+        }
+        System.Console.WriteLine("What are you offering for this item? Select your item for this trade: ");
+        i = 1;
+        foreach (Item ownItem in Item.itemsByUser[loggedInUser])
+        {
+            System.Console.WriteLine(i + ". " + loggedInUser + "\t" + ownItem.Name + "\t" + ownItem.Description);
+        }
+        if (!int.TryParse(Console.ReadLine() ?? "0", out itemSelection))
+        {
+            itemSelection = 0;
+        }
+        if (itemSelection == 0)
+        {
+            trade_State = Trade_State.SHOW_Menu;
+            System.Console.WriteLine("Trade canceled. ");
+            return;
+        }
+        System.Console.WriteLine("Type your own item's number or 0 to cancel the trade.");
+        userTrades.Add(new Trade(username, tradeItems[itemSelection - 1].Value, Item.itemsByUser[loggedInUser][itemSelection -1], Trade_State.PENDING_Trade)); // add trade to user trade list
+        System.Console.WriteLine("Trade request created and pending");
+    }
+
     public static void showHistory(string loggedInUser)
     {
         //TODO
